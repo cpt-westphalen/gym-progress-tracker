85fc4a4edfde4a4557cad1dee41bc834637999eb
rbwestphalen <rbwestphalen@gmail.com>
2023-02-27 15:23:13 -0300

    new: error handling for Register WorkoutSession use-case

--- mobile/src/domains/habit-tracker/useCases/RegisterWorkoutSession.ts
+++ mobile/src/domains/habit-tracker/useCases/RegisterWorkoutSession.ts
@@ -8,6 +8,9 @@ export class RegisterWorkoutSession {
 	constructor(private repo: WorkoutSessionRepository) {}
 
 	public exec({ date, details, userId, workout }: WorkoutSessionProps): void {
+		if (date == null || workout == null) {
+			throw new Error("Date and Workout must be provided");
+		}
 		const workoutSession = new WorkoutSession({
 			date,
 			details,
