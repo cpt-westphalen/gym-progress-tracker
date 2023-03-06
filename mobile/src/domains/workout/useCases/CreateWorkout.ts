1b4071580b34a7c7afb781e93f0c6b26bfb9e99a
rbwestphalen <rbwestphalen@gmail.com>
2023-03-06 17:36:13 -0300

    fix: CreateWorkout use-case now receives WorkoutProps

--- mobile/src/domains/workout/useCases/CreateWorkout.ts
+++ mobile/src/domains/workout/useCases/CreateWorkout.ts
@@ -1,12 +1,12 @@
 import { Workout, WorkoutProps } from "../entities/Workout";
 import { WorkoutRepository } from "../repositories/WorkoutRepository";
-import { v4 as randomUUID } from "uuid";
+import uuid from "react-native-uuid";
 
 export class CreateWorkout {
 	constructor(private repo: WorkoutRepository) {}
 
 	public exec({ id, muscleGroups, title }: WorkoutProps): Workout {
-		if (!id) id = randomUUID();
+		if (!id) id = uuid.v4() as string;
 		const workout = new Workout({ id, muscleGroups, title });
 		this.repo.save(workout);
 		return workout;
